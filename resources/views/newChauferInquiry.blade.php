1693ad773443065ba5eae10e4e4f59640dd2db01
Alex Brasser <alexbrasser@gmail.com>
2018-03-05 11:56:16 +0100

    added <br>

--- resources/views/newChauferInquiry.blade.php
+++ resources/views/newChauferInquiry.blade.php
@@ -3,7 +3,7 @@ Email: {{ $inquiry->email }} <br>
 Phone: {{ $inquiry->phone }} <br>
 <br>
 Date: {{ $inquiry->date_time }} <br>
-Private Car: @if($inquiry->private_car) Yes @else No @endif
+Private Car: @if($inquiry->private_car) Yes @else No @endif <br>
 Comments: {{ $inquiry->comments }} <br>
 Locations: <br>
 @foreach($inquiry->locations as $location)
