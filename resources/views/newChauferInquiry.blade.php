bd2f395091d95f228d5448fcd37c0c35b96e24d8
Alex Brasser <alexbrasser@gmail.com>
2018-03-05 11:53:23 +0100

    added private_car

--- resources/views/newChauferInquiry.blade.php
+++ resources/views/newChauferInquiry.blade.php
@@ -3,6 +3,7 @@ Email: {{ $inquiry->email }} <br>
 Phone: {{ $inquiry->phone }} <br>
 <br>
 Date: {{ $inquiry->date_time }} <br>
+Private Car: @if($inquiry->private_car) Yes @else No @endif
 Comments: {{ $inquiry->comments }} <br>
 Locations: <br>
 @foreach($inquiry->locations as $location)
